56ea3a1963f578d8aaecf6cfd77fab60bbc7f5c9
cseswapon <cseswapon@gmail.com>
2022-08-21 09:32:23 +0600

    [ADD] - negative and positive number added

---
+++ neg_pos.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int number=-52, value;
+    // printf("Enter Your Number");
+    // scanf("%d", &number);
+    if (number>0)
+    {
+        printf("This is a Positive Number");
+    }else{
+        printf("This is a Negative Number");
+    }
+    return 0;
+}
