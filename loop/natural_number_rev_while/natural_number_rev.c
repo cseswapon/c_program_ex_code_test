b15f73eef970030103b6e97a3a9f13b4922b2b86
cseswapon <cseswapon@gmail.com>
2022-08-23 00:27:24 +0600

    [ADD] - loop file something program added

---
+++ loop/natural_number_rev_while/natural_number_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n = 100;
+    int i = 1;
+    while (n >= i)
+    {
+        printf("i = %d\n", i);
+        i++;
+    }
+
+    return 0;
+}
---
+++ loop/natural_number_reverse/natural_number_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n = 100, i;
+    /* printf("Enter the number");
+    scanf("%d", &n); */
+    for (i = n; i >= 0; i--)
+    {
+        printf("\n i = %d", i);
+    }
+
+    return 0;
+}
